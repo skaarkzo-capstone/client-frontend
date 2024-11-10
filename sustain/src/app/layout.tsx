73b7d44c0ceb1253e273580d4fe5976ca5c4c45a
Zaid Okal <zokal@uwo.ca>
2024-11-10 00:09:18 -0500

    Added Home and sustain text with hyperlink for home + Added fonts

--- sustain/src/app/layout.tsx
+++ sustain/src/app/layout.tsx
@@ -2,15 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const ibarraRealNova = localFont({
+  src: "./fonts/IbarraRealNova-Regular.ttf",
+  variable: "--font-ibarra",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const aboreto = localFont({
+  src: "./fonts/Aboreto-Regular.ttf",
+  variable: "--font-aboreto",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibarraRealNova.variable} ${aboreto.variable} antialiased`}
       >
         {children}
       </body>
--- sustain/tailwind.config.ts
+++ sustain/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        ibarra: ["var(--font-ibarra)", "serif"],
+      },
     },
   },
   plugins: [],
