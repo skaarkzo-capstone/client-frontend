edc8e4047a15a46093b1d1dc238b67b6ae412581
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-14 22:08:35 -0500

    Created function to send company name to backend

--- sustain/src/app/hooks/EvaluateCompanySearch.ts
+++ sustain/src/app/hooks/EvaluateCompanySearch.ts
@@ -7,9 +7,27 @@ export default function EvaluateCompanySearch() {
     setInputValue(e.target.value);
   };
 
-  const logMessage = () => {
+  const logMessage = async () => {
     if (inputValue.trim() !== "") {
-      console.log(inputValue);
+      try {
+        const response = await fetch("http://localhost:8000/api/main/search/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ companyName: inputValue }),
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          console.log("Received from backend:", result);
+        } else {
+          console.error("Failed to retrieve data from backend");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+
       setInputValue("");
     }
   };
