f0390da3b20c33029096654daa881f4dd8bf103f
Arham Ansari <73043239+arhamansarii@users.noreply.github.com>
2024-12-04 21:08:04 -0500

    Update post company endpoint

--- src/app/hooks/utils/apiConfigs.ts
+++ src/app/hooks/utils/apiConfigs.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = "http://localhost:8000/api/main";
 
 export const API_ENDPOINTS = {
-  POST_COMPANY_SEARCH: `${API_BASE_URL}/company/complete-evaluation`,
+  POST_COMPANY_SEARCH: `${API_BASE_URL}/complete-evaluation`,
   FETCH_COMPANY_DATA: `${API_BASE_URL}/evaluated-companies`,
 };
