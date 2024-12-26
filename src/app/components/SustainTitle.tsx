d601ee112d8353d0c0e0124adfc80dbad0251c9a
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-12-26 16:59:42 -0500

    Separated footer into separate file

--- src/app/components/SustainTitle.tsx
+++ src/app/components/SustainTitle.tsx
@@ -1,14 +1,16 @@
 export default function SustainTitle() {
   return (
-    <h1
-      className="text-[67px] mb-8 tracking-widest"
-      style={{ fontFamily: "var(--font-aboreto)" }}
-    >
-      SUST
-      <span className="bg-gradient-to-b from-pink-500 to-blue-700 text-transparent bg-clip-text">
-        AI
-      </span>
-      N
-    </h1>
+    <>
+      <h1
+        className="text-[67px] mb-8 tracking-widest"
+        style={{ fontFamily: "var(--font-aboreto)" }}
+      >
+        SUST
+        <span className="bg-gradient-to-b from-pink-500 to-blue-700 text-transparent bg-clip-text">
+          AI
+        </span>
+        N
+      </h1>
+    </>
   );
 }
